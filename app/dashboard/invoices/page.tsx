a176fa13ecc16ed01f8b0519e7d7825064bdea36
alessandro stoppato <alexrah@gmail.com>
2024-11-11 21:42:13 +0100

    06: Setting Up Your Database

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,37 @@
-export default function InvoicesPage(){
+import {db, QueryResult} from '@vercel/postgres';
+import type { Invoice, Customer} from "@/app/lib/definitions";
+import {Fragment} from 'react';
+
+export default async function InvoicesPage(){
+
+  const client = await db.connect();
+  const invoices: QueryResult<Invoice & Customer > = await client.sql`SELECT * FROM invoices LEFT JOIN customers ON invoices.customer_id = customers.id`;
+
   return (
-    <p>Invoices Page</p>
+    <div>
+      <h1>Invoices Page</h1>
+      <table>
+        <thead>
+        <tr>
+          <td>ID</td>
+          <td>Customer</td>
+          <td>Amount</td>
+          <td>Date</td>
+          <td>Status</td>
+        </tr>
+        </thead>
+        <tbody>
+          {invoices.rows.map(invoice => (
+            <tr key={invoice.id}>
+              <td>{invoice.id}</td>
+              <td>{invoice.name}</td>
+              <td>{invoice.amount}</td>
+              <td>{typeof invoice.date}</td>
+              <td>{invoice.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
