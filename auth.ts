516940f0170f3dcb82af1edc9161e9cd7ed1d7bb
alessandro stoppato <alexrah@gmail.com>
2024-12-20 22:59:02 +0100

    15d: auth.ts: implement signIn logic

---
+++ auth.ts
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import {authConfig} from "@/auth.config";
+import {z} from 'zod';
+import {sql} from "@vercel/postgres";
+import type {User} from "@/app/lib/definitions";
+import bcrypt from "bcrypt";
+
+
+async function getUserByEmail(email: string) {
+  try {
+    const result = await sql<User>`
+      SELECT *
+      FROM users
+      WHERE email = ${email}
+    `;
+    return result.rows[0];
+
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+
+export const {signIn, signOut} = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: {label: "email", type: "text", placeholder: "jsmith"},
+        password: {label: "Password", type: "password"},
+      },
+      async authorize(credentials) {
+        const credentialsSchema = z.object({
+          email: z.string().email(),
+          password: z.string().min(6),
+        });
+        const parsedCredentials = credentialsSchema.safeParse(credentials);
+
+        if(parsedCredentials.success){
+          const {email, password} = parsedCredentials.data;
+          const userData = await getUserByEmail(email);
+          if(!userData) return null;
+          const passwordsMatch = await bcrypt.compare(password, userData.password);
+          if (passwordsMatch) return userData;
+        }
+        console.log('Invalid credentials');
+        return null
+      }
+    }),
+    ],
+})
