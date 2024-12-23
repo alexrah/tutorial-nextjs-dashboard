4f9a86efec5027a16ba9e0d687ffd9c77d5aa610
alessandro stoppato <alexrah@gmail.com>
2024-12-23 01:53:05 +0100

    SQL/queries.sql: check PG shared_buffers

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -7,3 +7,5 @@ SELECT * FROM invoices LEFT JOIN customers ON invoices.customer_id = customers.i
 UPDATE users SET capabilities = ARRAY['edit_assets'] WHERE id = '410544b2-4001-4271-9855-fec4b6a6442a';
 
 UPDATE users SET capabilities = array_append(capabilities, 'edit_posts') WHERE id = '410544b2-4001-4271-9855-fec4b6a6442a';
+
+SHOW shared_buffers;
