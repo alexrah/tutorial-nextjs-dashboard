53227827cc1827d3f04ff8c77c57672c0521aab0
alessandro stoppato <alexrah@gmail.com>
2024-12-23 01:52:06 +0100

    app/lib/utils.ts: memoize()

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -67,3 +67,18 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
     totalPages,
   ];
 };
+
+export function memoize<T extends (...args: any[]) => any>(func: T): T {
+  const cache = new Map<string, ReturnType<T>>();
+
+  return function(...args: Parameters<T>): ReturnType<T> {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      console.log('memoize key', key);
+      return cache.get(key)!;
+    }
+    const result = func(...args);
+    cache.set(key, result);
+    return result;
+  } as T;
+}
